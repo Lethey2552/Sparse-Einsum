16e321c0b7ef0c21224550a3f4541593e605c833
Lethey2552 <73588822+Lethey2552@users.noreply.github.com>
2024-04-19 12:29:27 +0200

    Added bmm contraction function - not finished

--- BMM/bmm_dense_einsum.py
+++ BMM/bmm_dense_einsum.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-# from .SQL.sql_sparse_einsum import sql_einsum_query_opt
 
-
-def find_idc_types(input_set, input_idc, output_set):
+def find_idc_types(input_set, input_idc, output_set, output_idc):
     triv_sum_idc = []
     contracted_idc = []
     contraction_idc = []
@@ -40,8 +38,51 @@ def find_idc_types(input_set, input_idc, output_set):
         [idx for idx in contraction_idc if idx in input_idc[0]] + contracted_idc
     order_right = batch_idc + contracted_idc + \
         [idx for idx in contraction_idc if idx in input_idc[1]]
+    order_output = batch_idc + \
+        [idx for idx in output_idc if idx not in contracted_idc]
 
-    return order_left, order_right
+    perm_AB = tuple(order_output.index(i) for i in output_idc)
+    if perm_AB == tuple(range(len(perm_AB))):
+        perm_AB = None
+
+    return order_left, order_right, order_output, perm_AB
+
+
+def single_einsum(eq: str, tensor: np.ndarray):
+    return np.einsum(eq, tensor)
+
+
+def bmm_contraction(tensors: dict, input_idc: list, results: tuple):
+    eq_a, eq_b = input_idc
+    target_order_A, target_order_B, target_order_out, perm_AB = results
+
+    # Left side
+    if eq_a is not None:
+        a = single_einsum(eq_a, tensors["A"])
+        print(eq_a)
+        print(a)
+    # if target_order_A is not None:
+    #     transpose_A = [current_order_A.index(dim) for dim in target_order_A]
+    #     a = np.transpose(tensors["A"], transpose_A)
+    print("\n")
+    # Right side
+    if eq_b is not None:
+        b = single_einsum(eq_b, tensors["B"])
+        print(eq_b)
+        print(b)
+    # if target_order_B is not None:
+    #     transpose_B = [current_order_B.index(dim) for dim in target_order_B]
+    #     b = np.transpose(tensors["B"], transpose_B)
+
+    ab = a @ b
+
+    # Output reshape
+    # if target_order_out is not None:
+    #     ab =
+    if perm_AB is not None:
+        ab = np.transpose(ab, perm_AB)
+
+    return ab
 
 
 def determine_transpose(order, target_order):
@@ -54,20 +95,12 @@ if __name__ == "__main__":
 
     tensor_names = ["A", "B"]
     tensors = {
-        "A": np.array([[[0, 1], [2, 3]], [[4, 5], [6, 7]]]),
-        "B": np.array([[[0, 1], [2, 3]], [[4, 5], [6, 7]]]),
+        "A": np.random.rand(2, 3, 4),
+        "B": np.random.rand(3, 2, 5),
     }
     arrays = [tensors["A"], tensors["B"]]
 
     # sql_einsum_query_opt(einsum_notation, tensor_names, tensors, arrays)
-
-    """
-    [[[ 2  3]
-      [ 6 11]]
-
-    [[46 55]
-     [66 79]]]
-  """
 
     # Get index lists and sets
     einsum_notation = einsum_notation.replace(" ", "")
@@ -76,12 +109,19 @@ if __name__ == "__main__":
     input_sets = set().union(*input_idc)
     output_sets = set(output_idc)
 
-    results = find_idc_types(input_sets, input_idc, output_sets)
-    target_order_A, target_order_B = results
+    results = find_idc_types(input_sets, input_idc, output_sets, output_idc)
+    target_order_A, target_order_B, target_order_out, _ = results
     current_order_A, current_order_B = input_idc
+    current_order_out = output_idc
 
-    transpose_A = [current_order_A.index(dim) for dim in target_order_A]
-    transpose_B = [current_order_B.index(dim) for dim in target_order_B]
+    transpose_out = [current_order_out.index(dim) for dim in target_order_out]
 
-    print(transpose_A)
-    print(transpose_B)
+    ab = bmm_contraction(tensors, input_idc, results)
+
+    print(ab)
+
+    # Get reference result
+    np_einsum = np.einsum(
+        einsum_notation, tensors["A"], tensors["B"])
+
+    print(f"\n\n{np.allclose(ab, np_einsum)}")
