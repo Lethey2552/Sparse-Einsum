ace73d2ac9b1efd970cc0e36bb5176cfde02a07b
Lethey2552 <73588822+Lethey2552@users.noreply.github.com>
2024-05-17 13:52:32 +0200

    Sparse matrix multiplication as base for bmm

--- BMM/bmm_sparse_einsum_trials.py
+++ BMM/bmm_sparse_einsum_trials.py
@@ -14,38 +14,75 @@ def get_2d_coo_matrix(mat: np.ndarray):
             coo_mat[1].append(j)
             coo_mat[2].append(mat[i][j])
 
-    return np.transpose(np.matrix(coo_mat))
+    return np.transpose(np.array(coo_mat))
+
+
+def coo_transpose(M, sort=True):
+    M[:, [1, 0]] = M[:, [0, 1]]
+
+    if sort:
+        M = M[np.lexsort((M[:, 1], M[:, 0]))]
+
+    return M
+
+
+def coo_matmul(A, B, debug=False):
+    B_T = coo_transpose(B.copy())
+
+    if debug:
+        print("A:")
+        print(A)
+        print()
+
+        print("B:")
+        print(B)
+        print()
+
+        print("B':")
+        print(B_T)
+        print()
+
+    C_dict = {}
+
+    for i in range(len(A)):
+        for j in range(len(B_T)):
+
+            if A[i, 1] == B_T[j, 1]:
+                key = (A[i, 0], B_T[j, 0])
+
+                if key not in C_dict:
+                    C_dict[key] = 0
+                C_dict[key] += A[i, 2] * B_T[j, 2]
+
+    C = [[], [], []]
+    for (i, j), v in C_dict.items():
+        C[0].append(i)
+        C[1].append(j)
+        C[2].append(v)
+
+    return np.transpose(np.array(C))
 
 
 if __name__ == "__main__":
     A = np.array([
-        [0, 0, 0, 0, 9, 0],
-        [0, 8, 0, 0, 0, 0],
-        [4, 0, 0, 2, 0, 0],
-        [0, 0, 0, 0, 0, 5],
-        [0, 0, 2, 0, 0, 0]
+        [0, 0, 0, 0, 0],
+        [0, 0, 10, 4, 2],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 3, 0],
+        [0, 4, 2, 0, 0]
     ])
 
     B = np.array([
-        [0, 0, 4, 0 ,0],
-        [0, 8, 0, 0 ,0],
-        [0, 0, 0, 0 ,2],
-        [0, 0, 2, 0 ,0],
-        [9, 0, 0, 0 ,0],
-        [0, 0, 0, 5 ,0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 8, 0],
+        [0, 0, 0, 0, 3],
+        [0, 0, 2, 9, 0],
+        [0, 2, 7, 0, 0]
     ])
 
     A_coo = get_2d_coo_matrix(A)
     B_coo = get_2d_coo_matrix(B)
 
-    B_coo_swap = B_coo.copy()
-    B_coo_swap[:, [1, 0]] = B_coo_swap[:, [0, 1]]
-    B_coo_transp = sorted(B_coo_swap, key=itemgetter(0, 1))
-
-    print("B:\n")
-    print(B_coo)
-    print(B_coo_swap)
-    print(B_coo_transp)
+    print(coo_matmul(A_coo, B_coo))
     print()
-
     print(A @ B)
