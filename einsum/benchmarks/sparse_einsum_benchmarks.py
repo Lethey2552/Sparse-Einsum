fa1a23b1df01d247373a4c2c656d50f07f41fd8b
Lethey2552 <manthey-leon@web.de>
2024-06-17 12:58:30 +0200

    Benchmark with more than 2 tensors

--- einsum/benchmarks/sparse_einsum_benchmarks.py
+++ einsum/benchmarks/sparse_einsum_benchmarks.py
@@ -7,29 +7,37 @@ from timeit import default_timer as timer
 
 
 if __name__ == "__main__":
-    einsum_notation = "abcik,backj->abcij"
+    print_results = False
 
-    A = sparse.random((4, 11, 10, 3, 2), idx_dtype=int)
-    B = sparse.random((11, 4, 10, 2, 3), idx_dtype=int)
-    sparse_arrays = [A, B]
+    einsum_notation = "abcik,abcjr,abcrk->abcij"
+
+    A = sparse.random((20, 11, 10, 4, 2), idx_dtype=int)
+    B = sparse.random((20, 11, 10, 2, 3), idx_dtype=int)
+    C = sparse.random((20, 11, 10, 3, 2), idx_dtype=int)
+    sparse_arrays = [A, B, C]
 
     A = sparse.asnumpy(A)
     B = sparse.asnumpy(B)
+    C = sparse.asnumpy(C)
 
     A_coo = Coo_matrix.coo_from_standard(A)
     B_coo = Coo_matrix.coo_from_standard(B)
-    arrays = [A_coo, B_coo]
+    C_coo = Coo_matrix.coo_from_standard(C)
+    arrays = [A_coo, B_coo, C_coo]
 
     # Python
     tic = timer()
-    # python_res = A @ B
+    try:
+        python_res = A @ B @ C
+    except:
+        python_res = None
     toc = timer()
 
     python_time = toc - tic
 
     # Sparse
     tic = timer()
-    sparse_res = sparse.einsum(einsum_notation, sparse_arrays[0], sparse_arrays[1])
+    sparse_res = sparse.einsum(einsum_notation, sparse_arrays[0], sparse_arrays[1], sparse_arrays[2])
     toc = timer()
 
     sparse_time = toc - tic
@@ -41,9 +49,14 @@ if __name__ == "__main__":
 
     sparse_einsum_time = toc - tic
 
-    print(f"Shapes: Python - python_res.shape,    Sparse - {sparse_res.shape},    Sparse Einsum - {sparse_einsum_res.shape}")
+    print(f"Shapes: Python - {python_res.shape if python_res is not None else 'None'},    Sparse - {sparse_res.shape},    Sparse Einsum - {sparse_einsum_res.shape}")
     print(f"Results are correct: {compare_matrices(sparse_einsum_res, sparse.asnumpy(sparse_res))}")
 
-    print(f"Python result: {python_time}s")
-    print(f"Sparse result: {sparse_time}s")
-    print(f"Sparse Einsum result: {sparse_einsum_time}s")
+    print(f"Python time: {python_time}s")
+    print(f"Sparse time: {sparse_time}s")
+    print(f"Sparse Einsum time: {sparse_einsum_time}s")
+
+    if print_results:
+        print(f"Python result:\n{python_res}")
+        print(f"Sparse result:\n{sparse.asnumpy(sparse_res)}\n")
+        print(f"Sparse Einsum result:\n{sparse_einsum_res.coo_to_standard()}")
