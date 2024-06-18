f60500a9d1f63ac50a9fc380e5223d1b60bb7866
Lethey2552 <manthey-leon@web.de>
2024-06-18 17:48:38 +0200

    Added c++ files for calculating the matmul - not tested

---
+++ einsum/backends/BMM/cpp_methods/coo_methods.cpp
@@ -0,0 +1,44 @@
+#include "coo_methods.h"
+
+void coo_matmul(double* A_data, int A_rows, int A_cols,
+                double* B_data, int B_rows, int B_cols,
+                double** C_data, int* C_rows, int* C_cols) {
+
+    std::unordered_map<std::pair<int, int>, double, PairHash, PairEqual> C_dict;
+
+    // Iterate over A and B to perform matrix multiplication
+    for (int i = 0; i < A_rows; ++i) {
+        int row_A = static_cast<int>(A_data[i * A_cols]);
+        int col_A = static_cast<int>(A_data[i * A_cols + 1]);
+        double val_A = A_data[i * A_cols + 2];
+
+        for (int j = 0; j < B_rows; ++j) {
+            int row_B = static_cast<int>(B_data[j * B_cols]);
+            int col_B = static_cast<int>(B_data[j * B_cols + 1]);
+            double val_B = B_data[j * B_cols + 2];
+
+            if (col_A == row_B) {
+                auto key = std::make_pair(row_A, col_B);
+                C_dict[key] += val_A * val_B;
+            }
+        }
+    }
+
+    // Prepare output C_data as a 2D array
+    std::vector<std::tuple<int, int, double>> result_data;
+    for (const auto& [key, value] : C_dict) {
+        result_data.push_back(std::make_tuple(key.first, key.second, value));
+    }
+
+    std::sort(result_data.begin(), result_data.end());
+
+    *C_rows = result_data.size();
+    *C_cols = 3;
+
+    *C_data = new double[*C_rows * *C_cols];
+    for (size_t i = 0; i < result_data.size(); ++i) {
+        (*C_data)[i * *C_cols + 0] = std::get<0>(result_data[i]);
+        (*C_data)[i * *C_cols + 1] = std::get<1>(result_data[i]);
+        (*C_data)[i * *C_cols + 2] = std::get<2>(result_data[i]);
+    }
+}
