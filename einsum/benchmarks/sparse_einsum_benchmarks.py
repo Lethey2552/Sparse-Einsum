df2c89947166619bf77336fbcb08ba8b5dce30b4
Lethey2552 <manthey-leon@web.de>
2024-06-26 18:34:55 +0200

    Added more example problems to test single_einsum

--- einsum/benchmarks/sparse_einsum_benchmarks.py
+++ einsum/benchmarks/sparse_einsum_benchmarks.py
@@ -6,13 +6,14 @@ from einsum.utilities.classes.coo_matrix import Coo_matrix
 from timeit import default_timer as timer
 
 if __name__ == "__main__":
-    print_results = False
+    print_results = True
+    run_np = False
 
-    einsum_notation = "tbacik,abcrk,bacjr->abcij"
+    # einsum_notation = "tbacik,sabcrk,ubacjr->abcij"
 
-    A = sparse.random((5, 20, 40, 10, 4, 2), density=0.2, idx_dtype=int)
-    B = sparse.random((40, 20, 10, 3, 2), density=0.2, idx_dtype=int)
-    C = sparse.random((20, 40, 10, 7, 3), density=0.2, idx_dtype=int)
+    # A = sparse.random((25, 20, 60, 10, 4, 2), density=0.1, idx_dtype=int)
+    # B = sparse.random((30, 60, 20, 10, 3, 2), density=0.1, idx_dtype=int)
+    # C = sparse.random((40, 20, 60, 10, 7, 3), density=0.1, idx_dtype=int)
 
     # einsum_notation = "abcik,abckr,abcrj->abcij"
 
@@ -20,48 +21,54 @@ if __name__ == "__main__":
     # B = sparse.random((2, 2, 3, 2, 2), density=0.5, idx_dtype=int)
     # C = sparse.random((2, 2, 3, 2, 2), density=0.5, idx_dtype=int)
 
-    sparse_arrays = [A, B, C]
+    einsum_notation = "bjii->jii"
 
-    A = sparse.asnumpy(A)
-    B = sparse.asnumpy(B)
-    C = sparse.asnumpy(C)
+    A = sparse.random((2, 2, 2, 2), random_state=0, density=1.0, idx_dtype=int)
 
-    A_coo = Coo_matrix.coo_from_standard(A)
-    B_coo = Coo_matrix.coo_from_standard(B)
-    C_coo = Coo_matrix.coo_from_standard(C)
-    arrays = [A_coo, B_coo, C_coo]
+    sparse_arrays = [A]
+    dense_arrays = []
+    sparse_einsum_arrays = []
 
-    # Numpy Dense
-    tic = timer()
-    numpy_res = np.einsum(einsum_notation, A, B, C)
-    toc = timer()
+    for i in sparse_arrays:
+        dense_arrays.append(sparse.asnumpy(i))
 
-    python_time = toc - tic
+    print(dense_arrays[0])
+
+    for i in dense_arrays:
+        sparse_einsum_arrays.append(Coo_matrix.coo_from_standard(i))
+
+    if run_np:
+        # Numpy Dense
+        tic = timer()
+        numpy_res = np.einsum(einsum_notation, *dense_arrays)
+        toc = timer()
+
+        numpy_time = toc - tic
 
     # Sparse
     tic = timer()
     sparse_res = sparse.einsum(
-        einsum_notation, sparse_arrays[0], sparse_arrays[1], sparse_arrays[2])
+        einsum_notation, *sparse_arrays)
     toc = timer()
 
     sparse_time = toc - tic
 
     # Sparse Einsum
     tic = timer()
-    sparse_einsum_res = sparse_einsum(einsum_notation, arrays)
+    sparse_einsum_res = sparse_einsum(einsum_notation, sparse_einsum_arrays)
     toc = timer()
 
     sparse_einsum_time = toc - tic
 
-    print(f"Shapes: Numpy - {numpy_res.shape if numpy_res is not None else 'None'},    Sparse - {sparse_res.shape},    Sparse Einsum - {sparse_einsum_res.shape}")
+    print(f"Shapes: Numpy - {numpy_res.shape if run_np else 'None'},    Sparse - {sparse_res.shape},    Sparse Einsum - {sparse_einsum_res.shape}")
     print(f"""Results are correct:\n    Sparse Einsum - Sparse: {compare_matrices(sparse_einsum_res, sparse.asnumpy(sparse_res))}
-    Sparse Einsum - Numpy: {compare_matrices(sparse_einsum_res, numpy_res)}""")
+    Sparse Einsum - Numpy: {compare_matrices(sparse_einsum_res, numpy_res) if run_np else 'None'}""")
 
-    print(f"Numpy time: {python_time}s")
+    print(f"Numpy time: {numpy_time if run_np else 'None'}s")
     print(f"Sparse time: {sparse_time}s")
     print(f"Sparse Einsum time: {sparse_einsum_time}s")
 
     if print_results:
-        print(f"Numpy result:\n{numpy_res}")
+        print(f"Numpy result:\n{numpy_res if run_np else 'None'}")
         print(f"Sparse result:\n{sparse.asnumpy(sparse_res)}\n")
         print(f"Sparse Einsum result:\n{sparse_einsum_res.coo_to_standard()}")
