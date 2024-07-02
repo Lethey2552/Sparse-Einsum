ac45e2ae895a36ea47f41ff7a9ff947ee874efdf
Lethey2552 <73588822+Lethey2552@users.noreply.github.com>
2024-07-02 11:46:36 +0200

    Compiling with correct args for operating system (GCC and MSVC)

--- einsum/backends/BMM/cpp_methods/setup.py
+++ einsum/backends/BMM/cpp_methods/setup.py
@@ -1,13 +1,25 @@
 import numpy as np
+import platform
 from setuptools import Extension, setup
 from Cython.Build import cythonize
+
+extra_compile_args = []
+op_name = platform.system()
+
+# TODO: try compiling with linux subsystem gcc
+
+if op_name == "Linux":
+    extra_compile_args = ['-fopenmp', '-O2', '-mavx2', '-std=c++17']
+elif op_name == "Windows":
+    extra_compile_args = ['/openmp:experimental',
+                          '/O2', '/arch:AVX2', '/std:c++17']
 
 extensions = [
     Extension(
         "einsum.backends.BMM.cpp_methods.coo_methods_lib",
         sources=["./einsum/backends/BMM/cpp_methods/coo_methods_lib.pyx",
                  "./einsum/backends/BMM/cpp_methods/coo_methods.cpp"],
-        include_dirs=[np.get_include()],  # include the numpy headers
+        include_dirs=[np.get_include(), "./libs/ips4o/include"],
         language="c++",
     )
 ]
